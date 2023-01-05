5cc3ce77685beb086f49e58577eb1ce6cf8dd6de
Monica Bacatan <monicab3002@outlook.com>
2023-01-04 22:34:24 -0800

    Removed useNewUrlParser in mongodb connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,7 +6,7 @@ const uri = 'mongodb+srv://admin:1HoY7VICevfUI4KI@cluster0.i1ubgrw.mongodb.net/?
 
 // mongodb+srv://admin:<password>@cluster0.i1ubgrw.mongodb.net/test
 // password: 1HoY7VICevfUI4KI
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-})
-
+// mongoose.connect(uri, {
+//     useNewUrlParser: true,
+// })
+mongoose.connect(uri)
