16dab201c585f3b80f738566a93cb7274ae16e06
Monica Bacatan <monicab3002@outlook.com>
2023-01-04 22:37:35 -0800

    Added function brackets in server.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 // const uri = 'mongodb+srv://admin:1HoY7VICevfUI4KI@cluster0.i1ubgrw.mongodb.net/test';
-const uri = 'mongodb+srv://admin:1HoY7VICevfUI4KI@cluster0.i1ubgrw.mongodb.net/?retryWrites=true&w=majority';
-// const uri = 'mongodb+srv://admin:1HoY7VICevfUI4KI@cluster0.i1ubgrw.mongodb.net/WebRTC4941';
+// const uri = 'mongodb+srv://admin:1HoY7VICevfUI4KI@cluster0.i1ubgrw.mongodb.net/?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://admin:1HoY7VICevfUI4KI@cluster0.i1ubgrw.mongodb.net/WebRTC4941';
 
 // mongodb+srv://admin:<password>@cluster0.i1ubgrw.mongodb.net/test
 // password: 1HoY7VICevfUI4KI
-// mongoose.connect(uri, {
-//     useNewUrlParser: true,
-// })
-mongoose.connect(uri)
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+})
+// mongoose.connect(uri)
