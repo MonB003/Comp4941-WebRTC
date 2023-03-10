14f60ed5eec0c32f61c76afbf200fe606584b64e
Monica Bacatan <monicab3002@outlook.com>
2023-03-09 22:31:31 -0800

    Updated a file

--- src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-// mongodb+srv://admin:<password>@cluster0.i1ubgrw.mongodb.net/test
-// password: 1HoY7VICevfUI4KI
-const uri = 'mongodb+srv://admin:1HoY7VICevfUI4KI@cluster0.i1ubgrw.mongodb.net/WebRTC4941';
-
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-}) 
